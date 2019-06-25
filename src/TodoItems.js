336601d83fe96b873ce14d77231102572afc0e78
Irene Mutua <mutuamirene.gmail.com>
2019-06-25 17:02:06 +0300

    Modifications to the components

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,11 +1,30 @@
+
+
 import React from "react";
 
 export default class TodoItems extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            done: false
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+        var _done = !this.state.done
+        this.setState({
+            done: _done
+        })
+    }
     render() {
         return (
-            <ul>
+            <ul className="theList">
                 {this.props.items.map((item, i) => {
                     return <li key={item}>
+                        <input
+                            checked={this.state.done}
+                            onChange={this.handleChange}
+                            type="checkbox" />
                         {item}
                         <button onClick={this.props.handleDelete.bind(null, item)}>
                             x
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,15 +1,75 @@
+/* import React from "react";
+import TodoItems from "./TodoItems";
+import "./TodoList.css";
 
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            items: []
+        };
+        this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+    }
+    addItem(e) {
+
+        if (this._inputElement.value !== "") {
+            let newItem = {
+                text: this._inputElement.value,
+                key: Date.now()
+            };
+            this.setState((prevState) => {
+                return {
+                    items: prevState.items.concat(newItem)
+                };
+            });
+        }
+        this._inputElement.value = "";
+        console.log(this.state.items);
+        e.preventDefault();
+
+    }
+    deleteItem(key) {
+        let filteredItems = this.state.items.filter(function (item) {
+            return (item.key !== key);
+
+        });
+        this.setState({
+            items: filteredItems
+           
+        });
+    }
+
+    render() {
+        return (
+            <div className="todoListMain">
+                <div className="header">
+                    <form onSubmit={this.addItem}>
+                        <input ref={(a) => this._inputElement = a} placeholder="input task" />
+                        <button type="submit">Add Task</button>
+                    </form>
+
+                </div>
+
+                <TodoItems entries={this.state.items}
+                    delete={this.deleteItem}/>
+
+            </div>
+        )
+    }
+}
+
+export default TodoList; */
 
 import React from "react";
 import TodoItems from "./TodoItems";
 import "./TodoList.css"
-
 export default class TodoList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             text: "",
-            items:[]
+            items:["eggs", "beans", "Bread"]
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -39,7 +99,8 @@ export default class TodoList extends React.Component {
     }
     render() {
         return (
-            <div>
+            <div className="todoListMain">
+                <div className="header">
                 <p>TODOLIST</p>
                 <form onSubmit={this.handleSubmit}>
                     <input onChange={this.handleChange} value={this.state.text}/>
@@ -49,7 +110,8 @@ export default class TodoList extends React.Component {
                 <TodoItems
                     handleDelete={this.handleDelete}
                     items={this.state.items} />
-        </div>
+                </div>
+                </div>
     )
 }
 }
