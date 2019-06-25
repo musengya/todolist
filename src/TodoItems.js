e74781ff857edc91db46ab1c41cdca20f0068eb9
Irene Mutua <mutuamirene.gmail.com>
2019-06-25 17:37:29 +0300

    Updated the component

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,37 +1,20 @@
 
 
 import React from "react";
+import DisplayTodoItem from "./DisplayTodoItem";
 
 export default class TodoItems extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            done: false
-        }
-        this.handleChange = this.handleChange.bind(this);
-    }
-    handleChange(event) {
-        var _done = !this.state.done
-        this.setState({
-            done: _done
-        })
-    }
     render() {
         return (
             <ul className="theList">
                 {this.props.items.map((item, i) => {
-                    return <li key={item}>
-                        <input
-                            checked={this.state.done}
-                            onChange={this.handleChange}
-                            type="checkbox" />
-                        {item}
-                        <button onClick={this.props.handleDelete.bind(null, item)}>
-                            x
-                            </button>
-                    </li>
+                    return <DisplayTodoItem
+                        key={item}
+                        handleDelete={this.props.handleDelete.bind(null, item)}
+                        item={item} />
                 })
                 }
+              
             </ul>
         )
     }
